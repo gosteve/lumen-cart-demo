31be75549d9fe8670eb1f1f84f55b8cad7adfd92
Steven <steve@codeplay.co.za>
2021-10-07 22:04:10 +0200

    Added routes for keys, carts and cart items

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+/**
+ * Request to generate a unique key for client applications.
+ */
+$router->get('/key/grant', 'KeyController@grant');
+$router->get('/key/revoke', 'KeyController@revoke');
+
+/**
+ * Cart Routes
+ */
+$router->get('/cart/{id}', ['middleware' => 'auth', 'uses' => 'CartController@show']);
+$router->post('/cart', ['middleware' => 'auth', 'uses' => 'CartController@store']);
+$router->put('/cart/{id}', ['middleware' => 'auth', 'uses' => 'CartController@update']);
+$router->delete('/cart/{id}', ['middleware' => 'auth', 'uses' => 'CartController@delete']);
+
+/**
+ * Cart Item Routes
+ */
+$router->get('/cartitem/{id}', ['middleware' => 'auth', 'uses' => 'CartItemController@show']);
+$router->post('/cartitem', ['middleware' => 'auth', 'uses' => 'CartItemController@create']);
+$router->put('/cartitem/{id}', ['middleware' => 'auth', 'uses' => 'CartItemController@update']);
+$router->delete('/cartitem/{id}', ['middleware' => 'auth', 'uses' => 'CartItemController@delete']);
